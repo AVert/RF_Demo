dd68c0b6a8d29fe71307792432cebf2e41838847
Nall11G <nall11sf@gmail.com>
2018-10-29 16:41:54 +0530

    Update Pythonexe.bat

--- TestResults/Pythonexe.bat
+++ TestResults/Pythonexe.bat
@@ -1,3 +1,3 @@
-pybot E:\Robot_Framework\Salesforce_Automation-Demo\TestCases\RegressionTestSuite.robot & python E:\Robot_Framework\Salesforce_Automation-Demo\TestResults\robotmetrics.py
+pybot TestCases\RegressionTestSuite.robot & python TestResults\robotmetrics.py
 
 
